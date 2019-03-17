35b36d130d94b01b2a02c90692cbe9ee0120d67c
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-03-17 03:01:48 +0200

    go backend - improved trace, made it produce a file for each step

--- k-distribution/include/go/interpreter/trace.go
+++ k-distribution/include/go/interpreter/trace.go
@@ -9,18 +9,34 @@ import (
 	"fmt"
 	m "%INCLUDE_MODEL%"
 	"os"
+	filepath "path/filepath"
 	"time"
 )
 
 var traceEnabled = false
+var traceName string
 var traceFile *os.File
 var traceWriter *bufio.Writer
 
 func initializeTrace() {
 	traceEnabled = true
 	formattedNow := time.Now().Format("20060102150405")
+	traceName = "trace_" + formattedNow
 	var err error
-	traceFile, err = os.OpenFile("trace_"+formattedNow+".log",
+	err = os.Mkdir(traceName, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error while creating trace directory:" + err.Error())
+	}
+	newTraceFile(traceName + "_init.log")
+}
+
+func newTraceFile(fileName string) {
+	if traceFile != nil {
+		traceWriter.Flush()
+		traceFile.Close()
+	}
+	var err error
+	traceFile, err = os.OpenFile(filepath.Join(traceName, fileName),
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("Error while creating trace file:" + err.Error())
@@ -50,6 +66,7 @@ func traceStepStart(stepNr int, currentState m.K) {
 	if !traceEnabled {
 		return
 	}
+	newTraceFile(fmt.Sprintf("%s_step%d.log", traceName, stepNr))
 	traceWriter.WriteString(fmt.Sprintf("\n\nstep #%d begin\n\n", stepNr))
 }
 
@@ -65,12 +82,15 @@ func traceNoStep(stepNr int, currentState m.K) {
 	if !traceEnabled {
 		return
 	}
-	traceWriter.WriteString(fmt.Sprintf("\nstep #%d end, no more steps\n", stepNr))
+	traceWriter.WriteString(fmt.Sprintf("\nstep #%d end, no more steps\n\n", stepNr))
+	traceWriter.WriteString(m.PrettyPrint(currentState))
 }
 
 func traceRuleApply(ruleType string, stepNr int, ruleInfo string) {
 	if !traceEnabled {
 		return
 	}
-	traceWriter.WriteString(fmt.Sprintf("rule %s #%-3d %s\n", ruleType, stepNr, ruleInfo))
+	if ruleType == "STEP" {
+		traceWriter.WriteString(fmt.Sprintf("rule %s #%-3d %s\n", ruleType, stepNr, ruleInfo))
+	}
 }
