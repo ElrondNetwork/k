e0772dcbfeb15b8d2ae01b696799ccbd327af04e
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-05-08 09:44:40 +0300

    go backend - collection match utility functions

--- k-distribution/include/go/model/util_collections.go
+++ k-distribution/include/go/model/util_collections.go
@@ -4,6 +4,42 @@ import (
 	"sort"
 )
 
+// ExtractKApplyArgs ... checks that a K item is a KApply and returns its arguments if so
+func ExtractKApplyArgs(subject K, expectedLabel KLabel, expectedArity int) ([]K, bool) {
+	kapp, isKapp := subject.(*KApply)
+	if !isKapp {
+		return nil, false
+	}
+	if kapp.Label != expectedLabel || len(kapp.List) != expectedArity {
+		return nil, false
+	}
+	return kapp.List, true
+}
+
+// ExtractListData ... checks that a K item is a list and returns its contents if so
+func ExtractListData(subject K, expectedSort Sort, expectedLabel KLabel) ([]K, bool) {
+	klist, isList := subject.(*List)
+	if !isList {
+		return nil, false
+	}
+	if klist.Sort != expectedSort || klist.Label != expectedLabel {
+		return nil, false
+	}
+	return klist.Data, true
+}
+
+// ExtractMapData ... checks that a K item is a map and returns its contents if so
+func ExtractMapData(subject K, expectedSort Sort, expectedLabel KLabel) (map[KMapKey]K, bool) {
+	kmap, isMap := subject.(*Map)
+	if !isMap {
+		return nil, false
+	}
+	if kmap.Sort != expectedSort || kmap.Label != expectedLabel {
+		return nil, false
+	}
+	return kmap.Data, true
+}
+
 // MapKeyValuePair ... just a pair of key and value that was stored in a map
 type MapKeyValuePair struct {
 	KeyAsString string
